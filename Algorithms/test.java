f906a869b407e30c18aad8adad7e7b2967f685de
QiLinXue <la.qcumber@gmail.com>
2018-03-10 17:18:57 -0500

    test

--- Algorithms/test.java
+++ Algorithms/test.java
@@ -1,7 +1,15 @@
-class test{
-    public static void main(String[] args) {
-        int[] arr = {1,4,5,0,9,3,938,21,485};
-        QuickSort.sort(arr);
-        System.out.println(arr[0]);
-    }
+import java.util.*;
+
+public class test{
+	public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list.get(0)); // prints "Hello!"
+
+        for (Integer s : list) {
+            System.out.print(s);
+        } // prints "Hello!", "How are you?"
+	}
 }
