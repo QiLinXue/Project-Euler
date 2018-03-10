f9408e9765d8acd850634cf5ba1b768cb406f085
QiLinXue <la.qcumber@gmail.com>
2018-03-10 13:56:54 -0500

    update

--- Project Euler/Problem10.java
+++ Project Euler/Problem10.java
@@ -8,16 +8,12 @@ class Problem10{
     //Sieve of Eratosthenes
     static void sieveOfEratosthenes(int n){
         long primeSum = 0L;
-		// Create a boolean array "prime[0..n]" and initialize
-		// all entries it as true. A value in prime[i] will
-		// finally be false if i is Not a prime, else true.
 		boolean prime[] = new boolean[n+1];
 		for(int i=0;i<n;i++)
 			prime[i] = true;
 
 		for(int p = 2; p*p <=n; p++)
 		{
-			// If prime[p] is not changed, then it is a prime
 			if(prime[p] == true)
 			{
 				// Update all multiples of p
