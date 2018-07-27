0ad0e9c49cd2da8e5ff89931d3902469e724e571
QiLinXue <la.qcumber@gmail.com>
2018-07-27 18:11:25 -0400

    alt method

--- Java/Problem015.java
+++ Java/Problem015.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 /*
 
 Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner. How many such routes are there through a 20×20 grid?
@@ -5,10 +7,13 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to t
 */
 
 // 07/26/2018: 538600 ns
+// 07/27/2018 (alt method): 568500 ns
 
 class Problem015{
     public static void main(String[] args) {
         long startTime = System.nanoTime();
+
+        // altMethod();
 
         //Start Code
         long[][] paths = new long[21][21];
@@ -23,5 +28,13 @@ class Problem015{
         long endTime   = System.nanoTime();
         long totalTime = endTime - startTime;
         System.out.println(totalTime);
+    }
+
+    //Alternative Code using Combinatorics
+    static void altMethod(){
+        int steps = 40;
+        long product = 1;
+        for(int i=steps/2+1;i<=steps;i++) product *= (i)/(i-steps/2);
+        System.out.println(product);
     }
 }
